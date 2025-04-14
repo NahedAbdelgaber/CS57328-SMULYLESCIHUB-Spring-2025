279718f58c75931a1e8d7c3a0633c7f01d4e9377
AyawnKhan <ayank@smu.edu>
2025-04-13 23:42:31 -0500

    Team 2 Sprint

---
+++ team2/Sprint 1/backend/main/java/software_engineering_project/entities/IndustryChallenge.java
@@ -0,0 +1,64 @@
+package software_engineering_project.entities;
+
+import jakarta.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "industry_challenge")
+public class IndustryChallenge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private String description;
+    private Integer budget;
+    private Date start_date;
+    private Date end_date;
+    private String required_expertise;
+    private Boolean open_status;
+
+    @Column(name = "feedback")
+    private String feedback;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    public void setId(Integer new_id) { this.id = new_id; }
+    public Integer getId() { return this.id; }
+
+    public void setTitle(String new_title) { this.title = new_title; }
+    public String getTitle() { return this.title; }
+
+    public void setDescription(String new_description) { this.description = new_description; }
+    public String getDescription() { return this.description; }
+
+    public void setBudget(Integer new_budget) { this.budget = new_budget; }
+    public Integer getBudget() { return this.budget; }
+
+    public void setStartDate(Date new_date) { this.start_date = new_date; }
+    public Date getStartDate() { return this.start_date; }
+
+    public void setEndDate(Date new_date) { this.end_date = new_date; }
+    public Date getEndDate() { return this.end_date; }
+
+    public void setRequiredExpertise(String new_expertise) { this.required_expertise = new_expertise; }
+    public String getRequiredExpertise() { return this.required_expertise; }
+
+    public void setOpenStatus(Boolean new_status) { this.open_status = new_status; }
+    public Boolean getOpenStatus() { return this.open_status; }
+
+    public String getFeedback() {
+        return feedback;
+    }
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
---
+++ team2/Sprint 1/backend/test/java/software_engineering_project/SoftwareEngineeringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package software_engineering_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SoftwareEngineeringProjectApplication.class)
+class SoftwareEngineeringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
